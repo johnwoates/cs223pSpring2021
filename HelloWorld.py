1a54d85b1d9dc6570cc89db6f64ff4d88f1535b9
johnwoates <77648852+johnwoates@users.noreply.github.com>
2021-01-22 14:10:50 -0800

    Changed dir structure
    Changed directory structure

--- HelloWorld.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-John Oates
-CPSC 223P-01
-1/25/2021
-joates@fullerton.edu
-"""
-
-# This is my first Python program. It greets the whole world.
---
+++ Lab-1/HelloWorld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: johnwoates
+@class:  CPSC 223P-01
+@date:   1/25/2021
+@email:  joates@fullerton.edu
+"""
+
+# This is my first Python program. It greets the whole world.
