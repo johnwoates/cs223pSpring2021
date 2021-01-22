6e0ac9a64c5a01fb6e0e90240952cb3a09c03236
johnwoates <77648852+johnwoates@users.noreply.github.com>
2021-01-22 09:23:26 -0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-@author: johnwoates
-@class:  CPSC 223P-01
-@date:   1/25/2021
-@email:  joates@fullerton.edu
+John Oates
+CPSC 223P-01
+1/25/2021
+joates@fullerton.edu
 """
 
 # This is my first Python program. It greets the whole world.
